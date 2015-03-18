c0e3c250b9d89454a137be6ae243629edc2ad49a
Filippo Mantovani <slashgibson97@gmail.com>
2015-03-18 23:02:14 +0100

    Manca metodo contains

--- src/cerchio/Circle.java
+++ src/cerchio/Circle.java
@@ -1,5 +1,60 @@
 package cerchio;
 
 public class Circle {
+	private double xC;
+	private double yC;
+	private double r;
+	
+	public double getXC() {
+		return xC;
+	}
+	public void setXC(double xC) {
+		this.xC = xC;
+	}
+	public double getYC() {
+		return yC;
+	}
+	public void setYC(double yC) {
+		this.yC = yC;
+	}
+	public double getR() {
+		return r;
+	}
+	public void setR(double r) {
+		if (r<=0)
+			throw new IllegalArgumentException("Il raggio deve essere positivo");
+		else
+			this.r=r;
+	}
+	
+	public Circle(double xC, double yC, double r){
+		this.xC=xC;
+		this.yC=yC;
+		if (r<=0)
+			throw new IllegalArgumentException("Il raggio deve essere positivo");
+		else
+			this.r=r;
+		
+	}
+	
+	public double getXMin(){
+		return this.xC - this.r;
+	}
+	
+	public double getXMax(){
+		return this.xC - this.r;
+	}
+	
+	public double getYMin(){
+		return this.yC - this.r;
+	}
+	
+	public double getYMax(){
+		return this.yC + this.r;
+	}
+	
+	public boolean contains(Circle c){
+		
+	}
 
 }
